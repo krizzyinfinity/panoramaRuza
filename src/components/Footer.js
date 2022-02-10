bd050abec8c654e1a5f26a2fa31d9cff655214c8
krizzyinfinity <kristina.dobrotic@gmail.com>
2022-02-10 13:43:02 +0000

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import { FaEnvelope, FaPhone, FaAddressCard, FaFacebookSquare } from 'react-icons/fa';
+import {
+  FaEnvelope,
+  FaPhone,
+  FaAddressCard,
+  FaFacebookSquare
+} from 'react-icons/fa';
 
-export default function Footer({ footer }) {
-return <footer className={footer}>
-<h4>Contact us</h4>
+export default function Footer({
+  footer
+}) {
+  return <footer className={footer}>
+    <h4> Contact us </h4>
 
 
-<FaPhone /> <p className="myp2"> 00385981805406</p>
+    <FaPhone /> <p className="myp2" > 00385981805406 </p>
 
-<FaEnvelope />
-<p className="myp2">kristinamouly@outlook.com</p>
-<FaAddressCard />
-<p className="myp2"> Dragove 12 a</p>
+    <FaEnvelope />
+    <p className="myp2" > kristinamouly @outlook.com </p>
+    <FaAddressCard />
+    <p className="myp2" > Dragove 12 a </p>
 
-<p className="myp2">23286 Dugi otok</p>
+    <p className="myp2" > 23286 Dugi otok </p>
 
-<p>Follow us: </p>
-<a href="https://www.facebook.com/panoramaapartmentsCroatia">
-    <FaFacebookSquare icon={FaFacebookSquare} className="myIcon" />      
-  </a>          
+    <p > Follow us: </p>
+    <a href="https://www.facebook.com/panoramaapartmentsCroatia" >
+      <FaFacebookSquare icon={
+        FaFacebookSquare
+      }
+        className="myIcon" />
+    </a>
 
-</footer>;
+  </footer>;
 
 }
 
